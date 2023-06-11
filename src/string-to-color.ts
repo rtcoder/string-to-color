a206233c7e833d1554f1b1ce565d0fcdc76cb005
Dawid Jeż <dawidjez@gmail.com>
2023-06-11 16:53:00 +0200

    update

--- src/string-to-color.ts
+++ src/string-to-color.ts
@@ -1,15 +1,15 @@
 export function stringToColor(str: string): string {
-    let hash: number = 0;
-    for (let i = 0; i < str.length; i++) {
-        hash = str.charCodeAt(i) + ((hash << 5) - hash);
-    }
+  let hash: number = 0;
+  for (let i = 0; i < str.length; i++) {
+    hash = str.charCodeAt(i) + ((hash << 5) - hash);
+  }
 
-    let color: string = '#';
-    for (let i = 0; i < 3; i++) {
-        const value: number = (hash >> (i * 8)) & 0xFF;
-        const hex: string = ('00' + value.toString(16));
-        color += hex.substring(hex.length - 2);
-    }
+  let color: string = "#";
+  for (let i = 0; i < 3; i++) {
+    const value: number = (hash >> (i * 8)) & 0xff;
+    const hex: string = "00" + value.toString(16);
+    color += hex.substring(hex.length - 2);
+  }
 
-    return color;
+  return color;
 }
